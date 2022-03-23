af87930d01a99da32b616e109fe46be590807c0f
WillMon23 <88592707+WillMon23@users.noreply.github.com>
2022-03-23 15:09:08 -0500

    Initialize DijkstraPathFinding

---
+++ raygame/NodeGraph.h
@@ -0,0 +1,40 @@
+#pragma once
+#include <Vector2.h>
+#include "DynamicArray.h"
+
+namespace NodeGraph
+{
+	struct Node;
+
+	struct Edge
+	{
+		Node* target;
+		float cost;
+	};
+
+	struct Node
+	{
+		MathLibrary::Vector2 position;
+
+		float gScore;
+		float hScore;
+		float fScore;
+		bool walkable = true;
+		unsigned int color = 0xFFFFFFFFF;
+
+		Node* previous;
+
+		DynamicArray<Edge> edges;
+	};
+
+	DynamicArray<Node*> findPath(Node* start, Node* end);
+
+	void drawGraph(Node* start);
+
+	void drawNode(Node* node, float size = 14);
+
+	void drawConnectedNodes(Node* node, DynamicArray<Node*>& drawnList);
+
+	void resetGraphScore(Node* start);
+	void resetConnectedNodes(Node* node, DynamicArray<Node*>& resetList);
+}
